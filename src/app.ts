73979a5f60c30a51c1df366dc9a766d42617f30c
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-03-12 14:01:41 -0300

    nodejs-em-typescript

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ export class App {
   constructor() {
     this.express = express();
     this.listen(this.porta);
+    this.database();
   }
 
   public getApp(): express.Application {
@@ -18,6 +19,18 @@ export class App {
   private listen(porta: number) {
     this.express.listen(porta, () => {
       console.log('Servidor iniciando na porta ' + this.porta);
+    }); 
+  }
+    
+  private database(): void {
+    mongoose.connect('mongodb+srv://ryanpedrozacloud:0eZMx4knmxZe3ZpY@cluster0.hjlaeig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("Conexão com o banco de dados estabelecida com sucesso.");
+    })
+    .catch(error => {
+      console.error("Erro ao conectar ao banco de dados:", error);
     });
   }
-}
+  }
